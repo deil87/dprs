7c9d4717bef9d661002bf9f4b452332f424f0657
Andrey Spiridonov <spirtik87@gmail.com>
2025-07-09 19:07:14 +0200

    Added two pytests

---
+++ tests/test_dprs.py
@@ -0,0 +1,20 @@
+from dprs import dprs
+
+def test_two_same():
+    """Test that is checking that score for same lists is zero."""
+    lista = range(10)
+    score, _ = dprs(lista,lista)
+    assert score == 0
+    
+    
+def test_rank_shift_direction():
+    """
+    Test checks that for a single ranking position it is important whether shift happens to the left or to the right. 
+    For the sake of test travelling distance should be the same.
+    Scores must be differet (bigger if shift is towards top of the ranking)
+    """
+    lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    list_shift_right = [0, 1, 2, 3, 4, 5, 6, 9, 8, 7]  # 7 <-> 9
+    list_shift_left = [0, 1, 2, 3, 4, 7, 6, 5, 8, 9]   # 7 <-> 5
+
+    assert dprs(lista,list_shift_right)[0] < dprs(lista,list_shift_left)[0]
